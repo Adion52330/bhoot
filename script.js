77ee06442ae13ac08616466b2c554c6a21c1d904
Adion52330 <85357798+Adion52330@users.noreply.github.com>
2021-06-09 20:50:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function bar() {
+  document.getElementById("im1").src = "assets/5.jpg";
+}
+function iron() {
+  document.getElementById("im2").src = "assets/6.jpg";
+}
+function elsa() {
+  document.getElementById("im3").src = "assets/7.jpg";
+}
+function cap() {
+  document.getElementById("im4").src = "assets/8.jpg";
+}
+function re() {
+  location.reload();
+}
